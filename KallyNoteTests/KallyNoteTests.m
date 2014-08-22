e666838d9004b405603de9284e8e84df051e29a7
谢传奇 <xiechuanqi@123feng.com>
2014-08-22 10:18:59 +0800

    Initial Commit

---
+++ KallyNoteTests/KallyNoteTests.m
@@ -0,0 +1,34 @@
+//
+//  KallyNoteTests.m
+//  KallyNoteTests
+//
+//  Created by Zombie on 8/22/14.
+//  Copyright (c) 2014 Xcqwan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KallyNoteTests : XCTestCase
+
+@end
+
+@implementation KallyNoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
